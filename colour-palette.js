4bb85b79701ab7f62581a9ae46d224033b48e6a6
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-09-10 17:00:05 +0100

    add selected elements, hex value generator button event listener, hex value generator function .js

--- colour-palette.js
+++ colour-palette.js
@@ -1,6 +1,24 @@
-// Select elements
 
+// Select elements
+const hexId = document.querySelectorAll('.hex_id');
+const colour = document.querySelectorAll('.colour');
+const hexValueGeneratorButton = document.querySelector('#hexValueGeneratorButton');
 
 // Function to create random hex value
 
-console.log('rabbitz');
+function hexValueGenerator() { 
+    colour.forEach(element => {
+        const hexValue = Math.floor(Math.random()*16777215).toString(16);
+    element.style.background = "#" + hexValue;
+    });
+
+    hexId.forEach(element => {
+        const hexValue = Math.floor(Math.random()*16777215).toString(16);
+    element.innerHTML = "#" + hexValue;
+    });
+    
+}
+
+// Event listeners
+
+hexValueGeneratorButton.addEventListener('click', hexValueGenerator);
